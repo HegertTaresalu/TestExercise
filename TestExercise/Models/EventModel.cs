233942d2612ab0fe32fb14774ada29bd6649d732
Hegert Taresalu <herkuti@gmail.com>
2022-09-22 20:17:40 +0300

    add event model

---
+++ TestExercise/Models/EventModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TestExercise.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public string? EventLocation { get; set; }
+
+        public string? ExtraInfo { get; set; }
+    }
+}
